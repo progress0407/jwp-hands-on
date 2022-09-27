1a85c2113a8520c65b7c43d809ac29b2c517009c
progress0407 <progress0407@gmail.com>
2022-09-27 18:33:22 +0900

    style: clean style

--- di/src/test/java/nextstep/study/di/stage4/annotations/Stage4Test.java
+++ di/src/test/java/nextstep/study/di/stage4/annotations/Stage4Test.java
@@ -1,25 +1,11 @@
 package nextstep.study.di.stage4.annotations;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import nextstep.study.User;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 class Stage4Test {
-
-    @Test
-    void qwewqe() throws Exception {
-        final Constructor<UserService> constructor = UserService.class.getDeclaredConstructor();
-        constructor.setAccessible(true);
-        final UserService userService = constructor.newInstance();
-        final InMemoryUserDao inMemoryUserDao = new InMemoryUserDao();
-        for (final Field field : userService.getClass().getDeclaredFields()) {
-            field.setAccessible(true);
-            field.set(userService, inMemoryUserDao);
-        }
-    }
 
     @Test
     void stage4() {
@@ -34,7 +20,7 @@ class Stage4Test {
     }
 
     private static DIContext createDIContext() {
-        final var rootPackageName = Stage4Test.class.getPackage().getName();
+        final String rootPackageName = Stage4Test.class.getPackage().getName();
         return DIContext.createContextForPackage(rootPackageName);
     }
 }
